cac2da813e2a1843f65b0d00437c0e3283c859a8
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-07-29 16:34:29 +0200

    -a

---
+++ osModule.js
@@ -0,0 +1,30 @@
+const os = require('node:os')
+
+console.log(os)
+
+function getOs () {
+
+const nombre = os.platform(),
+      tipo =os.type(),
+      version = os.version(),
+      arquitectura = os.arch(),
+     cpus= cpus().length,
+     mTotal= (os.totalmem() / (1024 ** 2)).toFixed(2) + "MB",
+    mLibre= (os.freemem() / (1024 ** 2)).toFixed(2) + " MB"
+
+
+
+}
+
+return {
+
+  nombre: nombre
+  tipo : tipo
+   version : version
+   arquitectura: arquitectura
+   cpus: cpus
+   mTotal: ` ${mTotal} MB`
+   mLibre:` ${mLibre} MB`
+}
+
+module.exports = getOs;
